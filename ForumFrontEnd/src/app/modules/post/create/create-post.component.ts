4294256a023c85dd93eac22a9c301bf3a1e06086
Cuong Bui <cuongbphv@gmail.com>
2018-12-15 11:46:46 +0700

    update teamplate create post

--- ForumFrontEnd/src/app/modules/post/create/create-post.component.ts
+++ ForumFrontEnd/src/app/modules/post/create/create-post.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 import { TranslateService } from '../../../core/services';
 
@@ -7,9 +7,30 @@ import { TranslateService } from '../../../core/services';
   templateUrl: './create-post.component.html',
   styleUrls: ['./create-post.component.scss']
 })
-export class CreatePostComponent {
+
+export class CreatePostComponent implements OnInit {
 
   content: any = {};
+
+  ngOnInit() {
+    let es = document.querySelectorAll('.input-categories');
+    for (let i = 0; i < es.length; i++) {
+      es[i]._list = es[i].querySelector('ul');
+      es[i]._input = es[i].querySelector('input');
+      es[i]._input._icategories = es[i];
+      es[i].onkeydown = function(e){
+        var e = event || e;
+        if(e.keyCode == 13) {
+          var c = e.target._icategories;
+          var li = document.createElement('li');
+          li.innerHTML = c._input.value;
+          c._list.appendChild(li);
+          c._input.value = '';
+          e.preventDefault();
+        }
+      }
+    }
+  }
 
   constructor(
     public translate: TranslateService
